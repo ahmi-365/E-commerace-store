c1713c53e5a1615e6f389104dd322ef2256ef428
ahmi-365 <mohdahmedraza70@gmail.com>
2024-11-04 14:55:47 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,22 +3,14 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', // Ensure this matches the output directory
-  },
   server: {
     proxy: {
       '/api': {
-        target: 'https://e-commerace-store.onrender.com', // Use the Render URL
-        changeOrigin: true, // Changes the origin of the host header to the target URL
-        secure: false, // If your API server uses HTTPS and has a valid certificate, keep this as true.
-        // If you encounter issues with self-signed certificates, you can set it to false during development.
-      },
-    },
-  },
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
+        target: 'https://m-store-server-ryl5.onrender.com', // Correct backend URL
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 });
